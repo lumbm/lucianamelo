ef15179a817a15319e474eff050551e3d86e754d
Luciana Melo <124807051+lumbm@users.noreply.github.com>
2023-10-17 16:29:23 -0300

    Add files via upload

---
+++ execicio.py
@@ -0,0 +1,7 @@
+class Lapis:
+    def __init__(self, modelo, cor, tamanho, ponta):
+        self.modelo = modelo
+        self.cor = cor
+        self.tamanho = tamanho
+        self.ponta = ponta
+        print(self.modelo, self.cor, self.tamanho,)
