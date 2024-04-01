d14149cf339326e8ed17c2313db4ef8e257d3025
Luciana Melo <124807051+lumbm@users.noreply.github.com>
2024-04-01 14:07:53 -0300

    Delete execicio.py

--- execicio.py
+++
@@ -1,7 +0,0 @@
-class Lapis:
-    def __init__(self, modelo, cor, tamanho, ponta):
-        self.modelo = modelo
-        self.cor = cor
-        self.tamanho = tamanho
-        self.ponta = ponta
-        print(self.modelo, self.cor, self.tamanho,)
